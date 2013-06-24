ccdab3a45fa17bc5362b5f7acdd8c50fa2ce49b2
Kittipun <Boot191@gmail.com>
2013-06-25 00:19:34 +0700

    Update id3_parser.py
    add more comment

--- scripts/id3_parser.py
+++ scripts/id3_parser.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
+# ID3 tag parser
+
 import os
 import sys
 import eyed3
-import pymongo
 
 fileList = []
 rootdir = sys.argv[1]
-
-client = pymongo.MongoClient('localhost', 27017)
 
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
