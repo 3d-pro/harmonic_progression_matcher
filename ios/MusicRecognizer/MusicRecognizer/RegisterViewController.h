dbb2c873b74ab8a332b45c339f87dd4a5c481aa7
Kittpun <Boot191@gmail.com>
2013-12-30 11:14:07 +0700

    added more view controller and some rough design.

---
+++ ios/MusicRecognizer/MusicRecognizer/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  MusicRecognizer
+//
+//  Created by Kittipun on 12/30/13.
+//  Copyright (c) 2013 ITKMITL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
---
+++ ios/MusicRecognizer/MusicRecognizer/RegisterViewController.m
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.m
+//  MusicRecognizer
+//
+//  Created by Kittipun on 12/30/13.
+//  Copyright (c) 2013 ITKMITL. All rights reserved.
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
